af278c4a9c34eb8130dc775ed073ebfe346e7e94
Leonardo Pereira Sampaio <profleonardo95@gmail.com>
2023-07-16 21:45:53 -0300

    test pr

--- backend/tests/helpers.ts
+++ backend/tests/helpers.ts
@@ -15,7 +15,6 @@ export async function generateValidToken(user?: User) {
   const token = jwt.sign({ userId: incomingUser.id }, process.env.JWT_SECRET);
 
   await createSession(token);
-
   return token;
 */
 
